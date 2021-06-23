b19cc664fbf12c0ff194da22b34f5f95fd275297
Eniamart <eniamartinez0@gmail.com>
2021-06-23 00:14:26 -0600

    Agregando arquitectura básica

---
+++ Reposteria/Reposteria.BL/ProductosBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reposteria.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> ObtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Envinado Francés";
+            producto1.Precio = 200;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Tres Leches";
+            producto2.Precio = 250;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Mokaccino";
+            producto3.Precio = 180;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return listadeProductos;
+        }
+    }
+}
